1a2b3935de9f97471852d1e5f6049c098dec2e6c
Stelios Nicoletti <stelios@Stelios-Desktop.localdomain>
2022-09-06 21:24:13 +0300

    deployed to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  appVersion: require('../../package.json').version,
+  //appVersion: require('../../package.json').version,
   production: true,
   firebase: {
     apiKey: "AIzaSyCORXQ--ZINNVdEAEcomuSubVEIfS1fOz0",
