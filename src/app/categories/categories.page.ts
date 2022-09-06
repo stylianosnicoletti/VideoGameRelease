88f98a9c210fb1720c3271220cfe1b1e61a603c7
Stelios <stylianos.nicoletti@gmail.com>
2022-09-06 18:45:44 +0300

    got authtoken

--- src/app/categories/categories.page.ts
+++ src/app/categories/categories.page.ts
@@ -1,5 +1,10 @@
+// eslint-disable-next-line
+
+import { HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthToken } from '../interfaces/authToken';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-categories',
@@ -9,12 +14,33 @@ import { ActivatedRoute } from '@angular/router';
 export class CategoriesPage implements OnInit {
   public categories: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private apiService: ApiService
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     //needs a guard
     this.categories = this.activatedRoute.snapshot.paramMap.get('id');
     console.log(this.activatedRoute.snapshot.paramMap);
-  }
+    this.apiService.getAccessToken().subscribe((authToken) => {
+      console.log(authToken);
 
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          'Client-ID': 's6uz6vqq5yknu1a5gf5ycz5r7avp2c',
+          Authorization: `Bearer ${authToken.access_token}`,
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+          'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Client-ID, Authorization'
+        })
+      };
+
+      this.apiService.getGames(httpOptions).subscribe((data) => {
+        console.log(data);
+      });
+    });
+  }
 }
