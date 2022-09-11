e7c6f02d16cf60f1537b5059ffef7f94ad950301
Stelios Nicoletti <stelios@Stelios-Desktop.localdomain>
2022-09-11 23:20:43 +0300

    infinite scroll, removed console logs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'gameblast',
+  appName: 'videogamerelease',
   webDir: 'www',
   bundledWebRuntime: false
 };
